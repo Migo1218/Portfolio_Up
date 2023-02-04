e5909d01ca5aefc27d8b258d5cb874c5b92b037b
MiguelHdez18 <miguel.hernandez@ithealth.co>
2023-02-04 10:14:10 -0500

    fix: bg-fit

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -7,7 +7,7 @@ const Resume = () => {
   // });
   let tasks = [];
   return (
-    <div className="bg-white dark:bg-bluePortfolio md:h-screen py-6">
+    <div className="bg-white dark:bg-bluePortfolio md:h-fit py-6">
       <div className="max-w-7xl mt-12 mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className=" text-md font-body  font-semibold text-5xl text-bluePortfolio dark:text-iconColor">
           Experience
@@ -21,13 +21,15 @@ const Resume = () => {
                   <div className="md:grid grid-cols-2 w-full m-auto p-5">
                     {/* PRIMER DIV */}
                     <div className="md:border-r-4 border-slate-300">
-                      <p className="text-bluePortfolio font-semibold text-2xl mt-1">
+                      <p className="text-bluePortfolio dark:text-white font-semibold text-2xl mt-1">
                         {item.name}
                       </p>
-                      <p className="text-slate-700 italic text-lg mt-1">
+                      <p className="text-slate-700 italic dark:text-white text-lg mt-1">
                         {item.company} / {item.typeWork}
                       </p>
-                      <p className="text-slate-700 text-lg mt-1">{item.date}</p>
+                      <p className="text-slate-700 text-lg mt-1 dark:text-white">
+                        {item.date}
+                      </p>
                       <div className="bg-bluePortfolio rounded-md w-fit mt-2 inline-flex">
                         <FaBusinessTime className="text-white mt-2.5 ml-2" />
                         <h6 className="text-white font-semibold p-2">
@@ -37,11 +39,11 @@ const Resume = () => {
                     </div>
                     {/* SEGUNDO DIV */}
                     <div className="mt-5 md:mt-0 pl-4">
-                      <p className="text-bluePortfolio font-semibold text-2xl">
+                      <p className="text-bluePortfolio dark:text-white font-semibold text-2xl">
                         {item.company}
                       </p>
                       {item.tasks.map((task) => (
-                        <p className="text-slate-700 text-lg mt-1">
+                        <p className="text-slate-700 dark:text-white text-lg mt-1">
                           - {task.description}
                         </p>
                       ))}
